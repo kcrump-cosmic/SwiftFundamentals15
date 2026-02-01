816df6907fb3d3ec29fb8d772bfc4d1c0e506077
kcrump-cosmic <kcrump1983@gmail.com>
2026-02-01 14:35:58 -0500

    completed lab 1-9

---
+++ Unit1/Lesson1_9/InterfaceBuilderBasics/InterfaceBuilderBasics/InterfaceBuilderBasics/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  InterfaceBuilderBasics
+//
+//  Created by fall2025 on 2/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mainLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeTitle(_ sender: Any) {
+        mainLabel.text = "This app rocks!"
+    }
+    
+}
+
