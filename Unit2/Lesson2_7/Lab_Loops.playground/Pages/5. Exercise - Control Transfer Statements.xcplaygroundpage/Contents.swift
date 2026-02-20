2b6d4c2b51286212e849c15e36b2744aba46d603
kcrump-cosmic <kcrump1983@gmail.com>
2026-02-20 16:47:24 -0500

    completed page 5

--- Unit2/Lesson2_7/Lab_Loops.playground/Pages/5. Exercise - Control Transfer Statements.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_7/Lab_Loops.playground/Pages/5. Exercise - Control Transfer Statements.xcplaygroundpage/Contents.swift
@@ -5,9 +5,23 @@
  */
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+for (index, letter) in alphabet.enumerated() {
+    if index.isMultiple(of: 2) {
+        continue
+    }
+    print(letter)
+}
 
 //:  Create a `[String: String]` dictionary where the keys are names of states and the values are their capitals. Include at least three key/value pairs in your collection, with one of them being your home state. Now loop through this dictionary again, printing out the keys and values in a sentence, but add an if statement that will check if the current iteration is your home state. If it is, print("I found my home!") and break out of the loop.
 
+let states = ["California": "Sacramento", "North Carolina": "Raleigh", "Texas": "Austin"]
+
+for (state, capital) in states {
+    print("\(capital) is the capital of \(state)")
+    if state == "North Carolina" {
+        print("I found my home!")
+    }
+}
 
 /*:
 [Previous](@previous)  |  page 5 of 6  |  [Next: App Exercise - Finding Movements](@next)
