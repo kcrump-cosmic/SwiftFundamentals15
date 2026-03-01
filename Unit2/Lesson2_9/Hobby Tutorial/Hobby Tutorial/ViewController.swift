58ece840ab1fe3628fee86364a08e5e34ae11229
kcrump-cosmic <kcrump1983@gmail.com>
2026-03-01 13:21:38 -0500

    lab 2-9

---
+++ Unit2/Lesson2_9/Hobby Tutorial/Hobby Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hobby Tutorial
+//
+//  Created by fall2025 on 3/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Unit2/Lesson2_9/Hobby Tutorial/Hobby TutorialTests/Hobby_TutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hobby_TutorialTests.swift
+//  Hobby TutorialTests
+//
+//  Created by fall2025 on 3/1/26.
+//
+
+import Testing
+@testable import Hobby_Tutorial
+
+struct Hobby_TutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
